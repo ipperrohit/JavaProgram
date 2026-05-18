057c782cf6ccde72d5f9fd8ac4670110683305d1
ipperrohit <ipperrohit@gmail.com>
2026-05-18 09:30:05 +0530

    Addition

---
+++ addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Addition{
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter first value");
+     int First=sc.nextInt();
+     System.out.println("Enter second value");
+     int Sec=sc.nextInt();
+     int Ans=First+Sec;
+     System.out.println("Sum of First and Second is :"+Ans);
+    }
+}
