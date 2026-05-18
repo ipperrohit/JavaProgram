d9d8335be503a03bea4f70b1c196f8eef49ff67f
ipperrohit <ipperrohit@gmail.com>
2026-05-18 09:50:27 +0530

    Aa_to_Zz program

---
+++ Aa_to_Zz.java
@@ -0,0 +1,7 @@
+public class Aa_to_Zz{
+    public static void main(String args[]){
+        for(char ch1='A',ch2='a';ch1<='Z';ch1++,ch2++){
+            System.out.println(ch1+""+ch2);
+        }
+    }
+}
