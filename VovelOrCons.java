b2b966156b4a86bec567d4d92a7b411843fcd825
ipperrohit <ipperrohit@gmail.com>
2026-05-18 21:23:25 +0530

    VovelOrCons Program

---
+++ VovelOrCons.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class VovelOrCons{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      char ch=sc.next().charAt(0);
+      if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        System.out.println("Vovel");
+      }else{
+        System.out.println("Consonent");
+      }
+    }
+}
