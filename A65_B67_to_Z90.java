e46173c10a19c985505ef480e5b1f1918c255077
ipperrohit <ipperrohit@gmail.com>
2026-05-18 10:05:08 +0530

    A65_B67_to_Z90 pro

---
+++ A65_B67_to_Z90.java
@@ -0,0 +1,7 @@
+public class A65_B67_to_Z90{
+    public static void main(String[] args){
+        for(char ch='A',ch1='A';ch<='Z';ch++,ch1++){
+            System.out.println(ch+""+(int)ch1);
+        }
+    }
+}
