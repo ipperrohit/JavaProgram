4d47a9dab9280512ff0097bc1e519dfa13657d5c
ipperrohit <ipperrohit@gmail.com>
2026-05-18 09:42:47 +0530

    Multi program

---
+++ Multi.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Multi{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Num1");
+      int N1=sc.nextInt();
+      System.out.println("Enter N2");
+      int N2=sc.nextInt();
+      int Ans=N1*N2;
+      System.out.println("answer is : "+Ans);
+    }
+}
