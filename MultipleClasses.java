223e755d5c34875db17ed4d9ff2e37c1bec679b0
ipperrohit <ipperrohit@gmail.com>
2026-05-20 14:35:03 +0530

    MultipleClasses Program

---
+++ MultipleClasses.java
@@ -0,0 +1,20 @@
+public class MultipleClasses {
+    public static void Class1Method(){
+        System.out.println("This is Method 1");
+    }
+    class demo2{
+        public static void Demo2Method(){
+            System.out.println("This is mathod 2");
+        }
+    }
+    class demo3{
+        public static void Demo3Method(){
+            System.out.println("This is mathod 3");
+        }
+    }
+    public static void main(String args[]){
+       MultipleClasses.Class1Method();
+       demo2.Demo2Method();
+       demo3.Demo3Method();
+    }
+}
