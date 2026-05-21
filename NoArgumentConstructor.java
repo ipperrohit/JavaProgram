381b30634307f69bafa58575fa04a53e5fdfdd0b
ipperrohit <ipperrohit@gmail.com>
2026-05-21 22:48:05 +0530

    no Argument constructor Program

---
+++ NoArgumentConstructor.java
@@ -0,0 +1,12 @@
+class NoArgumentConstructor{
+
+    public NoArgumentConstructor(){
+        System.out.println("this is No Argument Constructor");
+        
+    }
+    
+    public static void main(String args[]){
+        NoArgumentConstructor obj=new NoArgumentConstructor();
+         
+    }
+}
