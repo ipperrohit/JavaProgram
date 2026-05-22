088134cc005b51e89a999bb35877f5bfd3242f8c
ipperrohit <ipperrohit@gmail.com>
2026-05-22 08:51:50 +0530

    Using this keyword program

--- Array.java
+++ Array.java
@@ -1,6 +1,28 @@
 import java.util.*;
+class Student{
+      String name;
+      int rollno;
+      char div;
+      boolean result;
+
+      Student(String name,int rollno,char div,boolean result){
+        this.name=name;
+        this.rollno=rollno;
+        this.div=div;
+        this.result=result;
+      }
+
+      public  void Display(){
+        System.out.println("name"+name);
+        System.out.println("rollno"+rollno);
+        System.out.println("div"+div);
+        System.out.println("result"+result);
+      }
+}
 public class Array {
+
     public static void main(String args[]){
-            System.out.println("Rohit");
+            Student obj=new Student("Rohit",53,'A',true);
+            obj.Display();
         }
 }
