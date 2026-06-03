fd4fabc272797ce503ee2d6cc5a1db8760e43226
ipperrohit <ipperrohit@gmail.com>
2026-06-03 11:13:31 +0530

    Finally block

---
+++ FinallyBlock.java
@@ -0,0 +1,12 @@
+public class FinallyBlock {
+    public static void main(String args[]){
+        try{
+            System.out.println(1/0);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        finally{
+            System.out.println("alwas execute");
+        }
+    }
+}
