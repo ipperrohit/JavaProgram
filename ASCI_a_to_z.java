1e52c9ad77db3b7b590e7a8a12f32db62d9d227f
ipperrohit <ipperrohit@gmail.com>
2026-06-07 22:14:00 +0530

    ASCI values of a to z character

---
+++ ASCI_a_to_z.java
@@ -0,0 +1,7 @@
+public class ASCI_a_to_z {
+    public static void main(String args[]){
+        for(char ch='a';ch<='z';ch++){
+            System.out.println((int )ch);
+        }
+    }
+}
--- Array.java
+++ Array.java
@@ -1,5 +1,7 @@
 public class Array {
     public static void main(String args[]){
-        System.out.println("Rohit");
+        for(int i=65;i<=90;i++){
+            System.out.println((char)i);
+        }
     }
 }
