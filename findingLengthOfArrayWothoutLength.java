b0649486ca804dc59882690a7eeabffb393556ac
ipperrohit <ipperrohit@gmail.com>
2026-06-08 08:53:54 +0530

    finding the length of an array without using length variable

---
+++ findingLengthOfArrayWothoutLength.java
@@ -0,0 +1,10 @@
+public class findingLengthOfArrayWothoutLength {
+    public static void main(String args[]){
+        int mark[]={1,2,3,4,56,7,6,7,8,9,0};
+        int len=0;
+        for(int i:mark){
+            len++;
+        }
+        System.out.println(len);
+    }
+}
