e9c09f9b341208f0d1d2fb0dd090c57072fe50d7
ipperrohit <ipperrohit@gmail.com>
2026-06-08 09:20:36 +0530

    print one D array using Do While loop

---
+++ OneDArrauUsingDWhileLoop.java
@@ -0,0 +1,10 @@
+public class OneDArrauUsingDWhileLoop {
+    public static void main(String args[]){
+        int[] mark={1,2,3,4,5};
+        int i=0;
+        do{
+         System.out.println(mark[i]);
+         i++;
+        }while(i<mark.length);
+    }
+}
