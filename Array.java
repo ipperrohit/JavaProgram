431f38f06b54ca8a1ffdeb0aeff0fc7533966ed6
ipperrohit <ipperrohit@gmail.com>
2026-06-08 09:35:15 +0530

    toString() method

--- Array.java
+++ Array.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Array {
     public static void main(String args[]){
-       int[] mark=new int[0];
-       System.out.println(mark.getClass());
+       int arr[]={1,2,3,4,5,6};
+       System.out.println(Arrays.toString(arr));
 
     }
 }
