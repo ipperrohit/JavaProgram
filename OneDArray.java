fe5ae789f978b896c2c8e1b2616ec69d8f1320de
ipperrohit <ipperrohit@gmail.com>
2026-06-08 09:17:03 +0530

    Printing of one dimensional array

---
+++ OneDArray.java
@@ -0,0 +1,8 @@
+public class OneDArray {
+    public static void main(String args[]){
+        int mark[]={1,2,3,4,5};
+        for(int i=0;i<mark.length;i++){
+            System.out.println(mark[i]);
+        }
+    } 
+}
