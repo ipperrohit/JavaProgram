ec5f479a877038d7daf3d914d9368d2862c55f0b
ipperrohit <ipperrohit@gmail.com>
2026-06-08 09:55:52 +0530

    Merge Two array

--- Array.java
+++ Array.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 
 public class Array {
     public static void main(String args[]){
-       int arr[]={1,2,3,4,5,6};
-       System.out.println(Arrays.toString(arr));
+       int[] a = {1,2,3,4,5}; 
+for(int i=0;i<10;i++) 
+{ 
+System.out.println(a[i]); 
+} 
 
     }
 }
