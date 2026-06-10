3b40dcbdb7be7dd147dba0b0e48f266d4988719c
ipperrohit <ipperrohit@gmail.com>
2026-06-10 08:59:22 +0530

    append() method are used to add char or string at end of the main String

---
+++ appenMethod.java
@@ -0,0 +1,7 @@
+public class appenMethod {
+    public static void main(String args[]){
+        StringBuffer st =new StringBuffer("Rohit");
+        st.append("Ipper");
+        System.out.println(st);
+    }
+}
