82c6ccc594b4550b8926c139fa62186aea9c81f4
ipperrohit <ipperrohit@gmail.com>
2026-06-10 09:44:12 +0530

    insert(index,string) method are used to add data at any index in string

---
+++ insertMethod.java
@@ -0,0 +1,6 @@
+public class insertMethod {
+    public static void main(String args[]){
+        StringBuffer str = new StringBuffer("Rohit");
+        System.out.println(str.insert(5,"Ipper"));
+    }
+}
